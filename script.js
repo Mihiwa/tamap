1dd3ad9ffc1bc9cefedf7f7b509c86c8055412dd
Mihiwa <jemhkym@gmail.com>
2024-11-24 21:47:17 +0000

    Starting the main view

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-import { faker } from "@faker-js/faker";
+import { faker } from "https://esm.sh/@faker-js/faker";
 
-const fakeName = faker.person.fullName();
-//Generate a random image URL
-const imageUrl = faker.image.avatar();
+// Caitlyn Kerluke
+const randomName = faker.person.fullName();
 
-console.log(fakeName);
+// Rusty@arne.info
+const randomImage = faker.image.avatar();
+const greeting = document.querySelector("#greetings");
+greeting.textContent = `Hello, ${randomName}! 👋`;
+const date = document.querySelector("#date");
+date.textContent = `Today, ${new Date().toDateString()}`;
 
-const profileImage = document.querySelector(".profile-image");
-profileImage.src = imageUrl;
-console.log(profileImage.src);
-const profileName = document.querySelector(".profile-name");
-profileName.textContent = fakeName;
-console.log(profileName);
+document.querySelector(".profile-name").textContent = randomName;
+document.querySelector("#profile-image").innerHTML += `
+        <img src="${randomImage}" alt="random image" id="image"/></span>
+      `;
+const assigned = document.querySelectorAll(".assigned-to");
+assigned.forEach((person) => {
+  person.innerHTML += `
+        <img src="${randomImage}" alt="random image" class="profile-image"/></span>
+      `;
+});
+const menuLis = document.querySelectorAll("li");
+menuLis.forEach((li) => {
+  li.addEventListener("click", () => {
+    menuLis.forEach((li) => li.classList.remove("active"));
+    li.classList.toggle("active");
+  });
+});
